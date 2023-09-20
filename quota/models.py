e5876aa9260524e9e0316ae4c96b15f53591072f
Jedtapat Tanthiew <6410685090@cn330>
2023-09-20 22:12:59 +0700

    add column

--- quota/models.py
+++ quota/models.py
@@ -5,7 +5,10 @@ from django.db import models
 class Course(models.Model):
     subject = models.CharField(max_length=64)
     subject_id = models.CharField(max_length=5)
+    semester = models.CharField(max_length=2)
+    year = models.CharField(max_length=4)
     seat = models.IntegerField()
+    available = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.subject_id}: {self.seat} seatleft"
