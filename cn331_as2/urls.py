ab494282ae87e345f8728e2e972080ce8568cff0
Jedtapat Tanthiew <6410685090@cn330>
2023-09-20 22:09:35 +0700

    add urls

--- cn331_as2/urls.py
+++ cn331_as2/urls.py
@@ -19,5 +19,7 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('course/', include('quota.urls'))
+    path('course/', include('quota.urls')),
+    path('users/' , include('users.urls')),
+    path('', include('users.urls')),
 ]
