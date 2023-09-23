763c61a7f59564ab4110eed4d5d607ab552621f1
Jedtapat Tanthiew <6410685090@cn330>
2023-09-23 21:13:48 +0700

    ลบไฟล์ทำfix all file

--- quota/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.mainpage, name="main"),
-    path('index', views.index, name="indexq"),
-    path('<int:id>' , views.info, name="info")
-]
