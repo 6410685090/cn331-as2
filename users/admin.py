2012ff2fb18a27bf474ad91258703766eb3c6879
Jedtapat Tanthiew <6410685090@cn330>
2023-10-07 14:12:14 +0700

    add adminsite view

--- users/admin.py
+++ users/admin.py
@@ -3,7 +3,9 @@ from .models import Student , Course
 # Register your models here.
 
 class CourseAdmin(admin.ModelAdmin):
+    list_display = ["subject_id" , "subject", "available", "seat"]
     filter_horizontal = ["student"]
+
 
 admin.site.register(Student)
 admin.site.register(Course,CourseAdmin)
