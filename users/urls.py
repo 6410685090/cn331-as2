5b8fa65f733efbf8aab855e4ea16878651545918
Jedtapat Tanthiew <6410685090@cn330>
2023-10-07 14:30:37 +0700

    change some path

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.login_view,),
     path('index', views.index, name="user"),
-    path('<str:courseid>/index',views.courseinfo,name='courseinfo'),
+    path('info/<str:courseid>',views.courseinfo,name='courseinfo'),
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
     path('signup', views.signup,name='signup'),
