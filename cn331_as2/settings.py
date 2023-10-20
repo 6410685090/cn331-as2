c765eae50c13eb791cf31538ebbda2e46b5eb0f6
Jedtapat Tanthiew <6410685090@cn330>
2023-10-21 01:22:37 +0700

    change some setting

--- cn331_as2/settings.py
+++ cn331_as2/settings.py
@@ -129,6 +129,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
